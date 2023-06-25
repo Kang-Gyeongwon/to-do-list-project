f48f2255dc6101d68df66bfd111e76f64332aa68
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-25 17:59:43 +0900

    feat: Header 기본 html 작성

--- src/components/home/Header/index.js
+++ src/components/home/Header/index.js
@@ -1,5 +1,21 @@
+import { Headline } from "./styled";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Headline>
+        <spna>My Todo List</spna>
+        <span>React</span>
+      </Headline>
+      <div>
+        <span>제목</span>
+        <input type="text" />
+        <span>내용</span>
+        <input type="text" />
+        <input type="submit" value="추가하기" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/home/Header/styled.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const Headline = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
