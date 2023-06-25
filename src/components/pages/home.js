389bbcddc1d8283fbc07ee2c8a966b925729ab6e
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-25 18:42:38 +0900

    docs: ToDoList 기본 폴더 세팅

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,11 @@
 import Header from "../home/Header";
+import ToDoList from "../home/ToDoList";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <ToDoList />
     </div>
   );
 };
