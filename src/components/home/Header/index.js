aa1c1c390b2988b1120203719d307f8665336d37
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-25 20:33:22 +0900

    feat: input 입력값 useState에 저장하기

--- src/components/home/Header/index.js
+++ src/components/home/Header/index.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import { Headline, InputBox, FormLabel, FormInput, AddBtn } from "./styled";
 
 const Header = () => {
+  const [todos, updateTodo] = useState([]);
+  const [todoId, updateTodoId] = useState(0);
+  const [curTilte, updateTitle] = useState("");
+  const [curBody, updateBody] = useState("");
+
+  const handleClickSubmit = () => {
+    if (todoId >= 0) {
+      updateTodo([
+        ...todos,
+        {
+          id: todoId,
+          title: curTilte,
+          body: curBody,
+          isDone: false,
+        },
+      ]);
+      updateTodoId(todoId + 1);
+      updateTitle("");
+      updateBody("");
+      console.log(curTilte, curBody);
+    }
+  };
+
   return (
     <div>
       <Headline>
@@ -9,10 +33,18 @@ const Header = () => {
       </Headline>
       <InputBox>
         <FormLabel>제목</FormLabel>
-        <FormInput type="text" />
+        <FormInput
+          type="text"
+          value={curTilte}
+          onChange={(e) => updateTitle(e.target.value)}
+        />
         <FormLabel>내용</FormLabel>
-        <FormInput type="text" />
-        <AddBtn type="submit" value="추가하기" />
+        <FormInput
+          type="text"
+          value={curBody}
+          onChange={(e) => updateBody(e.target.value)}
+        />
+        <AddBtn type="submit" value="추가하기" onClick={handleClickSubmit} />
       </InputBox>
     </div>
   );
