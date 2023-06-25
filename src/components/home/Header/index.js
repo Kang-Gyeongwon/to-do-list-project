dc690b3680913b9364c384592aaa673bd510844b
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-25 18:39:45 +0900

    style: Header style 지정

--- src/components/home/Header/index.js
+++ src/components/home/Header/index.js
@@ -1,4 +1,4 @@
-import { Headline } from "./styled";
+import { Headline, InputBox, FormLabel, FormInput, AddBtn } from "./styled";
 
 const Header = () => {
   return (
@@ -7,13 +7,13 @@ const Header = () => {
         <spna>My Todo List</spna>
         <span>React</span>
       </Headline>
-      <div>
-        <span>제목</span>
-        <input type="text" />
-        <span>내용</span>
-        <input type="text" />
-        <input type="submit" value="추가하기" />
-      </div>
+      <InputBox>
+        <FormLabel>제목</FormLabel>
+        <FormInput type="text" />
+        <FormLabel>내용</FormLabel>
+        <FormInput type="text" />
+        <AddBtn type="submit" value="추가하기" />
+      </InputBox>
     </div>
   );
 };
