e2753246564e871dd73fd6b507eaec99adbc54ec
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-25 18:48:00 +0900

    feat: todolist 기본 html 작성

--- src/components/home/ToDoList/index.js
+++ src/components/home/ToDoList/index.js
@@ -1,3 +1,31 @@
-const ToDoList = () => {}
+const ToDoList = () => {
+  return (
+    <div>
+      <div>
+        <h2>Working.. 🔥</h2>
+        <div>
+          <div>
+            <h2>리액트 공부하기</h2>
+            <spna>리액트 기초를 공부해봅시다</spna>
+          </div>
+          <div>
+            <button>삭제하기</button>
+            <button>완료</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>Done..! 🎉</h2>
+          <spna>리액트 기초를 공부해봅시다</spna>
+        </div>
+        <div>
+          <button>삭제하기</button>
+          <button>완료</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default ToDoList
