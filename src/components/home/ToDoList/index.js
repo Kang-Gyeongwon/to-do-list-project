2c847222a164ef3e8ec41b6bf7dba0c65baea65b
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-26 23:41:55 +0900

    feat: 완료 버튼 구현하기

--- src/components/home/ToDoList/index.js
+++ src/components/home/ToDoList/index.js
@@ -1,13 +1,61 @@
+import { useState } from "react";
 import { BtnBox, ToDoBtn, ToDoContainer, boxStyle } from "./styled";
 
 const ToDoList = (props) => {
   const todos = props.todos;
+  const [isDone, setIsDone] = useState(false);
+  const handleCompletionOnclick = (currentId) => {
+    todos.map((todo) => {
+      return todo.id !== currentId ? todo : setIsDone(!isDone);
+    });
+  };
+  const handleRemoveOnclick = (removeId) => {
+    // const todos.filter((todo) => {
+    //   return todo.id !== removeId
+    // })
+    // props.setTodops()
+  }
   return (
     <div>
       <div>
         <h2>Working.. 🔥</h2>
         <div style={boxStyle}>
           {todos.map((todo) => {
+            if (isDone === false) {
+              return (
+                <ToDoContainer key={todo.id}>
+                  <div>
+                    <h2>{todo.title}</h2>
+                    <spna>{todo.body}</spna>
+                  </div>
+                  <BtnBox>
+                    <ToDoBtn
+                      style={{ border: "2px solid red" }}
+                      onClick={() => {
+                        handleRemoveOnclick(todo.id)
+                      }}
+                    >
+                      삭제하기
+                    </ToDoBtn>
+                    <ToDoBtn
+                      style={{ border: "2px solid green" }}
+                      onClick={() => {
+                        handleCompletionOnclick(todo.id);
+                      }}
+                    >
+                      완료
+                    </ToDoBtn>
+                  </BtnBox>
+                </ToDoContainer>
+              );
+            }
+          })}
+        </div>
+      </div>
+      <div>
+        <h2>Done..! 🎉</h2>
+        {todos.map((todo) => {
+          if (isDone !== false) {
             return (
               <ToDoContainer key={todo.id}>
                 <div>
@@ -18,28 +66,18 @@ const ToDoList = (props) => {
                   <ToDoBtn style={{ border: "2px solid red" }}>
                     삭제하기
                   </ToDoBtn>
-                  <ToDoBtn style={{ border: "2px solid green" }}>완료</ToDoBtn>
+                  <ToDoBtn
+                    style={{ border: "2px solid green" }}
+                    onClick={() => {
+                      handleCompletionOnclick(todo.id);
+                    }}
+                  >
+                    완료
+                  </ToDoBtn>
                 </BtnBox>
               </ToDoContainer>
             );
-          })}
-        </div>
-      </div>
-      <div>
-        <h2>Done..! 🎉</h2>
-        {todos.map((todo) => {
-          return (
-            <ToDoContainer key={todo.id}>
-              <div>
-                <h2>{todo.title}</h2>
-                <spna>{todo.body}</spna>
-              </div>
-              <BtnBox>
-                <ToDoBtn style={{ border: "2px solid red" }}>삭제하기</ToDoBtn>
-                <ToDoBtn style={{ border: "2px solid green" }}>완료</ToDoBtn>
-              </BtnBox>
-            </ToDoContainer>
-          );
+          }
         })}
       </div>
     </div>
