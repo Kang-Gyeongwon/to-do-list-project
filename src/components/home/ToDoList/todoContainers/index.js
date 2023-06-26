13c9af29a80ecffcdcd96d54a8f2a57824a4c6d6
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-26 19:16:29 +0900

    feat: todolist 출력하기

--- src/components/home/ToDoList/todoContainers/index.js
+++ src/components/home/ToDoList/todoContainers/index.js
@@ -1,19 +1,25 @@
 import { BtnBox, ToDoBtn, ToDoContainer } from "./styled";
 
 const TodoContainer = (props) => {
-  const todos = props.todos
+  const todos = props.todos;
   console.log(todos);
   return (
-    <ToDoContainer>
-      <div>
-        <h2>{todos.title}</h2>
-        <spna>{todos.body}</spna>
-      </div>
-      <BtnBox>
-        <ToDoBtn style={{ border: "2px solid red" }}>삭제하기</ToDoBtn>
-        <ToDoBtn style={{ border: "2px solid green" }}>완료</ToDoBtn>
-      </BtnBox>
-    </ToDoContainer>
+    <div>
+      {todos.map((todo) => {
+        return (
+          <ToDoContainer key={todo.id}>
+            <div>
+              <h2>{todo.title}</h2>
+              <spna>{todo.body}</spna>
+            </div>
+            <BtnBox>
+              <ToDoBtn style={{ border: "2px solid red" }}>삭제하기</ToDoBtn>
+              <ToDoBtn style={{ border: "2px solid green" }}>완료</ToDoBtn>
+            </BtnBox>
+          </ToDoContainer>
+        );
+      })}
+    </div>
   );
 };
 
