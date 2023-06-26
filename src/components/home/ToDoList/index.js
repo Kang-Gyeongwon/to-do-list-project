6007ebd509a82b4f59dd8f0a6e3f6c9ea961bfa6
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-26 21:08:38 +0900

    docs: TodoList에 TodoContainser 합치기

--- src/components/home/ToDoList/index.js
+++ src/components/home/ToDoList/index.js
@@ -1,15 +1,46 @@
-import TodoContainer from "./todoContainers/index";
+import { BtnBox, ToDoBtn, ToDoContainer, boxStyle } from "./styled";
 
 const ToDoList = (props) => {
+  const todos = props.todos;
   return (
     <div>
       <div>
         <h2>Working.. 🔥</h2>
-        <TodoContainer todos={props.todos} />
+        <div style={boxStyle}>
+          {todos.map((todo) => {
+            return (
+              <ToDoContainer key={todo.id}>
+                <div>
+                  <h2>{todo.title}</h2>
+                  <spna>{todo.body}</spna>
+                </div>
+                <BtnBox>
+                  <ToDoBtn style={{ border: "2px solid red" }}>
+                    삭제하기
+                  </ToDoBtn>
+                  <ToDoBtn style={{ border: "2px solid green" }}>완료</ToDoBtn>
+                </BtnBox>
+              </ToDoContainer>
+            );
+          })}
+        </div>
       </div>
       <div>
         <h2>Done..! 🎉</h2>
-        <TodoContainer todos={props.todos}  />
+        {todos.map((todo) => {
+          return (
+            <ToDoContainer key={todo.id}>
+              <div>
+                <h2>{todo.title}</h2>
+                <spna>{todo.body}</spna>
+              </div>
+              <BtnBox>
+                <ToDoBtn style={{ border: "2px solid red" }}>삭제하기</ToDoBtn>
+                <ToDoBtn style={{ border: "2px solid green" }}>완료</ToDoBtn>
+              </BtnBox>
+            </ToDoContainer>
+          );
+        })}
       </div>
     </div>
   );
