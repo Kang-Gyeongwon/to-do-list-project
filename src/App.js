76658e5afc0e51d28813febfd50388381842116a
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-01 22:03:03 +0900

    style: GlobalStyle 적용하기 위한 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-import HomePage from "./components/pages/home";
+import HomePage from "./pages/home";
+import { GlobalStyle } from "./Globalstyle";
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <>
+      <GlobalStyle />
+      <HomePage />
+    </>
+  );
 };
 
 export default App;
