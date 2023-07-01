7310e677f7948d6fe2a312c9ab8938b5fc2176a3
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-01 22:04:14 +0900

    style: Globalstyle.jsx에 Globalstyle 지정해주기

---
+++ src/Globalstyle.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body{
+  @font-face {
+    font-family: 'KyoboHandwriting2021sjy';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2212@1.0/KyoboHandwriting2021sjy.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  font-family: 'KyoboHandwriting2021sjy';
+  background-color: #000000;
+  color: #E8E8E8;
+  }
+`;
