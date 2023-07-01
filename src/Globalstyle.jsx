dd51f27108e24e75f53064ddfc1ed66ecfa649b7
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-01 22:05:56 +0900

    style: 글꼴 변경

--- src/Globalstyle.jsx
+++ src/Globalstyle.jsx
@@ -3,12 +3,12 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   body{
   @font-face {
-    font-family: 'KyoboHandwriting2021sjy';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2212@1.0/KyoboHandwriting2021sjy.woff2') format('woff2');
+    font-family: 'Humanbumsuk';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2210-2@1.0/Humanbumsuk.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
-  }
-  font-family: 'KyoboHandwriting2021sjy';
+}
+  font-family: 'Humanbumsuk';
   background-color: #000000;
   color: #E8E8E8;
   }
